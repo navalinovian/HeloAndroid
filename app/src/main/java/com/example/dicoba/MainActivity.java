dda18d30bb4e25b624dca759b5d13785e4689925
navalinovian <viansatria57@gmail.com>
2019-09-05 14:17:39 +0700

    Activity lifecycle add OnStart() and OnStop()

--- app/src/main/java/com/example/dicoba/MainActivity.java
+++ app/src/main/java/com/example/dicoba/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dicoba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"App on Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"App on Stop", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
